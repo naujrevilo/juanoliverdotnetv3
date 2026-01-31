78c2d4a4b22e2b55df825cfd26c2067021fc0089
Nauj <14898430+naujrevilo@users.noreply.github.com>
2026-01-31 10:56:43 -0500

    fix(tina): temporarily disable TinaUserCollection to debug react minified error

--- tina/config.ts
+++ tina/config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
-      TinaUserCollection,
+      // TinaUserCollection,
       {
         name: "blog",
         label: "Blog Posts",
