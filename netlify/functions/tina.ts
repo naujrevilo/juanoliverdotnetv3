a035f39a659fbf11b715e01d4044539b50a30156
Nauj <14898430+naujrevilo@users.noreply.github.com>
2026-01-31 12:04:06 -0500

    fix: wrap tina handler in express to fix nextauth request compatibility

--- netlify/functions/tina.ts
+++ netlify/functions/tina.ts
@@ -1,7 +1,15 @@
+import express from "express";
+import cookieParser from "cookie-parser";
 import { TinaNodeBackend, LocalBackendAuthProvider } from "@tinacms/datalayer";
 import { AuthJsBackendAuthProvider, TinaAuthJSOptions } from "tinacms-authjs";
 import databaseClient from "../../tina/database";
 import serverless from "serverless-http";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
 
@@ -26,32 +34,14 @@ const tinaHandler = TinaNodeBackend({
   databaseClient,
 });
 
-const serverlessHandler = serverless(tinaHandler, {
+app.all("*", (req, res) => {
+  tinaHandler(req, res);
+});
+
+export const handler = serverless(app, {
   request: (req: any, event: any, context: any) => {
     console.log("Serverless Request created");
     console.log("Req URL:", req.url);
     console.log("Req method:", req.method);
   },
 });
-
-export const handler = async (event: any, context: any) => {
-  try {
-    console.log("Tina Function Start");
-    console.log("Branch:", process.env.GITHUB_BRANCH);
-    console.log("Repo:", process.env.GITHUB_REPO);
-    console.log("Has Mongo URI:", !!process.env.MONGODB_URI);
-    console.log(
-      "Has GitHub Token:",
-      !!process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
-    );
-    console.log("Is Local:", isLocal);
-
-    return await serverlessHandler(event, context);
-  } catch (e: any) {
-    console.error("Tina Function Error:", e);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message || "Unknown error" }),
-    };
-  }
-};
